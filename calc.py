9f0d6f6878220bd94e3929e68a0726bfe650402b
alex3025 <31188915+alex3025@users.noreply.github.com>
2018-08-13 21:53:53 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,42 +1,45 @@
 print("Calcolatrice\nby alex3025\n")
-save_result = "no"
+save_result = False
+
 def result_show():
-	if save_result == "si":
+	if save_result == True:
 		print("\nRisultato: " + result_2)
-	elif save_result == "no":
+	elif save_result == False:
 		print("\nRisultato: " + result_1)
+		
 while True:
 	try:
-		if save_result == "no":
+		if save_result == False:
 			num1 = float(input("Inserire il primo numero: "))
 		operator = input("Inserire il segno di operazione: ")
 		num2 = float(input("Inserire il secondo numero: "))
+		
 		if operator == "+":
-			if save_result == "no":
+			if save_result == False:
 				result_1 = str(num1 + num2)
 				result_show()
-			elif save_result == "si":
+			elif save_result == True:
 				result_2 = str(float(result_1) + num2)
 				result_show()
 		elif operator == "-":
-			if save_result == "no":
+			if save_result == False:
 				result_1 = str(num1 - num2)
 				result_show()
-			elif save_result == "si":
+			elif save_result == True:
 				result_2 = str(float(result_1) - num2)
 				result_show()
 		elif operator == "*":
-			if save_result == "no":
+			if save_result == False:
 				result_1 = str(num1 * num2)
 				result_show()
-			elif save_result == "si":
+			elif save_result == True:
 				result_2 = str(float(result_1) * num2)
 				result_show()
 		elif operator == "/":
-			if save_result == "no":
+			if save_result == False:
 				result_1 = str(num1 / num2)
 				result_show()
-			elif save_result == "si":
+			elif save_result == True:
 				result_2 = str(float(result_1) / num2)
 				result_show()
 		else:
@@ -44,7 +47,8 @@ while True:
 	except ZeroDivisionError:
 		print("\nImpossibile dividere per zero!\n")
 	save_result = input("\nVuoi salvare il risultato per il calcolo successivo? (si/no): ")
-	if save_result == "si":
+	
+	if save_result == True:
 		print("\nIl primo numero è stato sostituito con il risultato dell'operazione precedente.\n")
-	elif save_result == "no":
+	elif save_result == False:
 		print("\nIl risultato non verrà memorizzato.\n")
